0e66ec8cfbf651905c048ed7fc9fe5bfcb99c50a
Łukasz Gruner <lukasz@gruner.lu>
2020-09-22 01:06:03 +0200

    stuff

--- eglot.el
+++ eglot.el
@@ -2631,7 +2631,7 @@ code actions at point"
   `(:workspaceFolders
        [,@(cl-delete-duplicates
               (mapcar #'eglot--path-to-uri
-                  (let* ((root (project-root (eglot--project server)))
+                  (let* ((root (projectile-project-root (eglot--project server)))
                          (top root)
                          candidate)
                       (message root)
